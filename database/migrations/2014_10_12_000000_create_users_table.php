5de37190271893a7b8528609d0cbb8b407e35f53
javierovico <javierovico@gmail.com>
2020-06-10 19:51:01 -0400

    s

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name',200);
+            $table->string('last_name',200);
+            $table->string('username',200)->unique();
+            $table->string('name',200);
+            $table->string('email',200)->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password',200);
+            $table->string('status',200);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
